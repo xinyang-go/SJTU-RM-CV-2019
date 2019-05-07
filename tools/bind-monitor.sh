9b3178753f7500db530791813c66b5cd258f7403
xinyang <txy15436@gmail.com>
2019-05-07 23:25:34 +0800

    更新监视脚本绑定方式。

--- tools/bind-monitor.sh
+++ tools/bind-monitor.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-echo "$1/tools/monitor.sh \"$2/run --run-with-camera\" &" > $2/monitor-run
+echo "#!/bin/bash" > $2/monitor-run
+echo "$1/tools/monitor.sh \"$2/run --run-with-camera\"" >> $2/monitor-run
 chmod +x $2/monitor-run
