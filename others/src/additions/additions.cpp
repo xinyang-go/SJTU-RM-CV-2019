96e1802c9efc4b724e81dcbbb72f10853a0f8f11
sun <SJT_sjtu1998@163.con>
2019-07-16 16:38:32 +0800

    fix bug

--- others/src/additions/additions.cpp
+++ others/src/additions/additions.cpp
@@ -34,7 +34,7 @@ void uartReceive(Serial *pSerial) {
     LOGM(STR_CTR(WORD_LIGHT_WHITE, "data receive start!"));
     while (true) {
         memset(buffer, 0, sizeof(buffer));
-        pSerial->ReadData((uint8_t *) &buffer, sizeof(mcuData)+1);
+        pSerial->ReadData((uint8_t *) buffer, sizeof(mcuData)+1);
         if (buffer[sizeof(mcuData)] == '\n') {
             memcpy(&mcuData, buffer, sizeof(mcuData));
             LOGM("Get, state:%c, mark:%d!", mcuData.state, (int) mcuData.mark);
