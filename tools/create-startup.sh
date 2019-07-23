07b74d6d27d1c836434c4ae69548308ec4961e78
xinyang <lloi_yang@sjtu.edu.cn>
2019-07-23 17:02:13 +0800

    use cpu performance in startup-run.

--- tools/create-startup.sh
+++ tools/create-startup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "#!/bin/bash" > $2/startup-run
+echo "echo sjturm | sudo -S cpufreq-set -g performance" >> $2/startup-run
 echo "$1/tools/auto-pull.sh" >> $2/startup-run
 echo "gnome-terminal -- bash -c \"echo sjturm | sudo -S $1/tools/monitor.sh \\\"$2/run --run-with-camera --save-video --wait-uart --save-labelled-boxes\\\"\"" >> $2/startup-run
 chmod +x $2/startup-run
