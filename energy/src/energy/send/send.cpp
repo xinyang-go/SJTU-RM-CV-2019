6c5ee59843bae3a1619c609f79d1ca6c876de1cb
sun <SJT_sjtu1998@163.con>
2019-07-25 09:24:19 +0800

    energy change

--- energy/src/energy/send/send.cpp
+++ energy/src/energy/send/send.cpp
@@ -95,14 +95,13 @@ void Energy::sendTarget(Serial &serial, float x, float y, uint16_t z, uint16_t u
 
     x_tmp = static_cast<short>(x * (32768 - 1) / 100);
     y_tmp = static_cast<short>(y * (32768 - 1) / 100);
-    z_tmp = static_cast<short>(z * (32768 - 1) / 100);
     buff[0] = 's';
     buff[1] = static_cast<char>((x_tmp >> 8) & 0xFF);
     buff[2] = static_cast<char>((x_tmp >> 0) & 0xFF);
     buff[3] = static_cast<char>((y_tmp >> 8) & 0xFF);
     buff[4] = static_cast<char>((y_tmp >> 0) & 0xFF);
-    buff[5] = static_cast<char>((z_tmp >> 8) & 0xFF);
-    buff[6] = static_cast<char>((z_tmp >> 0) & 0xFF);
+    buff[5] = static_cast<char>((z >> 8) & 0xFF);
+    buff[6] = static_cast<char>((z >> 0) & 0xFF);
     buff[7] = static_cast<char>((u >> 8) & 0xFF);
     buff[8] = static_cast<char>((u >> 0) & 0xFF);;
     buff[9] = 'e';
